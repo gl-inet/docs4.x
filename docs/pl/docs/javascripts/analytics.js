16c09be8a503036810ca63d60ea09bce7140da20
GL-iNet-Leo.Lin <leo.lin@gl-inet.com>
2026-07-13 22:14:11 +0800

    fix: enable GA feedback tracking for Was this page helpful? widget on all locales

--- docs/pl/docs/javascripts/analytics.js
+++ docs/pl/docs/javascripts/analytics.js
@@ -30,3 +30,21 @@ script4.text = `
       gtag('js', new Date());
       gtag('config', 'G-34T6Q5NL0V');`
 document.getElementsByTagName('head')[0].appendChild(script4);
+
+document$.subscribe(function () {
+  var form = document.forms.feedback
+  if (form) {
+    form.hidden = false
+    form.querySelectorAll('[type=submit]').forEach(function (btn) {
+      btn.addEventListener('click', function (e) {
+        e.preventDefault()
+        var page = document.location.pathname
+        var value = this.getAttribute('data-md-value')
+        gtag('event', 'feedback', { page: page, data: value })
+        form.firstElementChild.disabled = true
+        var note = form.querySelector(".md-feedback__note [data-md-value='" + value + "']")
+        if (note) note.hidden = false
+      })
+    })
+  }
+})
